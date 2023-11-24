3d806edb8ed1a4165e744f018b7943a3579d4517
Максим Туркин <maksim.turkin.16@gmail.com>
2023-11-24 16:32:16 +0300

    Разметка калькулятора

---
+++ CalculatorVue/src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template class="calcInterface"> 
+    <div class="textbox">
+        <input type="text">
+    </div>
+    <div class="buttons">
+        <button class="but">MC</button>
+        <button class="but">MR</button>
+        <button class="but">M+</button>
+        <button class="but">M-</button>
+        <button class="but">%</button>
+        <button class="but">CE</button>
+        <button class="but">C</button>
+        <button class="but">ClearSymol</button>
+        <button class="but">¹/x</button>
+        <button class="but">x²</button>
+        <button class="but">²√x</button>
+        <button class="but">÷</button>
+        <button class="but">7</button>
+        <button class="but">8</button>
+        <button class="but">9</button>
+        <button class="but">×</button>
+        <button class="but">4</button>
+        <button class="but">5</button>
+        <button class="but">6</button>
+        <button class="but">-</button>
+        <button class="but">1</button>
+        <button class="but">2</button>
+        <button class="but">3</button>
+        <button class="but">+</button>
+        <button class="but">eˣ</button>
+        <button class="but">0</button>
+        <button class="but">,</button>
+        <button class="but" id="equal">=</button>
+        <button class="but">Sin</button>
+        <button class="but">Cos</button>
+        <button class="but">tg</button>
+        <button class="but">arctg</button>
+        <button class="but">log</button>
+        <button class="but">ln</button>
+        <button class="but">!</button>
+        <button class="but">xʸ</button>
+    </div> 
+</template>
+
+<style>
+    
+</style>
+
+
