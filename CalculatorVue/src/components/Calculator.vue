489998231460879448292956acd96d6357b2cc2f
Максим Туркин <maksim.turkin.16@gmail.com>
2023-12-04 16:06:47 +0300

    Переименовал названия классов, исправил работу с памятью

--- CalculatorVue/src/components/Calculator.vue
+++ CalculatorVue/src/components/Calculator.vue
@@ -1,46 +1,46 @@
 <template> 
-    <div class="calcInterface">
+    <div class="calculator_interface">
       <div class="textbox">
         <p>{{result}}</p>
       </div>
       <div class="line"></div>
       <div class="buttons">
-        <button @click="mcClick"  class="butMemory">MC</button>
-        <button @click="mrClick" class="butMemory">MR</button>
-        <button @click="mplusClick" class="butMemory">M+</button>
-        <button @click="mminusClick" class="butMemory">M-</button>
-        <button @click="percentClick" class="but">%</button>
-        <button @click="ceClick" class="but">CE</button>
-        <button @click="cClick" class="but">C</button>
-        <button @click="symbolClick" class="but symbol"><img class="imgClear" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAABb0lEQVR4nO2YQUoDQRBFn6BrQXBIQBeC4A1c5QoeQG+gC28QD+HOpUs14M6VpzCL5AAhIIoYQXAhtgi1aM0w6Wl7YqWtBw2BqWr606k/NQWGYRiGYfxvuoBTsF6Aa2AnRsQB8KFAhPPWE7BZR0QHeJPkY/6eFnAl57kMTdoGHiTpFD205UyTkOA1YCgJN8AyunCyKlkBbiWwD6yiDzdLyBJwLkHjugWlSciJBLwCu+jFVQnZF5t9B/YiNj8EioC4QmIbEdL5pc0eeTVVJaaQGCc5sZQK2QLu5cFZ5MbrwJ3sMRCLnBXz9U6IZUpISputEpNSxJSQJmy2TExqEd+E+DY7AjZIh18H/R+/Q8yglpBuwzbr30LKm1AhpMwAYsnvrzWvYm8FWrNq+/VrIrUYN68XYllhpxTjsm5Rsmoas2rjs/qwyuZTN6vhw6KMg54XeUDXBnpynoscRqaPsf2hliH2RG4iZZNrGIZhGAYa+AQu6kng3Jtj4AAAAABJRU5ErkJggg==" alt=""></button>
-        <button @click="sinClick" class="but">sin</button>
-        <button @click="cosClick" class="but">cos</button>
-        <button @click="tgClick" class="but">tg</button>
-        <button @click="acrTgClick" class="but">arctg</button>
-        <button @click="logClick" class="but">log</button>
-        <button @click="lnClick" class="but">ln</button>
-        <button @click="factorialClick" class="but">!</button>
-        <button @click="powClick" class="but">xʸ</button>
-        <button @click="reverseClick" class="but">¹/x</button>
-        <button @click="sqrClick" class="but">x²</button>
-        <button @click="sqrtClick" class="but">²√x</button>
-        <button @click="divideClick" class="operation">÷</button>
-        <button @click="sevenClick" class="but">7</button>
-        <button @click="eightClick" class="but">8</button>
-        <button @click="nineClick" class="but">9</button>
-        <button @click="multiplyClick" class="operation">×</button>
-        <button @click="fourClick" class="but">4</button>
-        <button @click="fiveClick" class="but">5</button>
-        <button @click="sixClick" class="but">6</button>
-        <button @click="subtractClick" class="operation">-</button>
-        <button @click="oneClick" @keyup.enter="keyOne" class="but">1</button>
-        <button @click="twoClick" class="but">2</button>
-        <button @click="threeClick" class="but">3</button>
-        <button @click="plusClick" class="operation">+</button>
-        <button @click="expClick" class="but">eˣ</button>
-        <button @click="zeroClick" class="but">0</button>
-        <button @click="dotClick" class="but">,</button>
-        <button @click="equalClick" class="but equal">=</button>
+        <button @click="mcClick"  class="button_memory">MC</button>
+        <button @click="mrClick" class="button_memory">MR</button>
+        <button @click="mplusClick" class="button_memory">M+</button>
+        <button @click="mminusClick" class="button_memory">M-</button>
+        <button @click="percentClick" class="button_basic">%</button>
+        <button @click="ceClick" class="button_basic">CE</button>
+        <button @click="cClick" class="button_basic">C</button>
+        <button @click="symbolClick" class="button_basic button_symbol"><img class="symbol_image" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAABb0lEQVR4nO2YQUoDQRBFn6BrQXBIQBeC4A1c5QoeQG+gC28QD+HOpUs14M6VpzCL5AAhIIoYQXAhtgi1aM0w6Wl7YqWtBw2BqWr606k/NQWGYRiGYfxvuoBTsF6Aa2AnRsQB8KFAhPPWE7BZR0QHeJPkY/6eFnAl57kMTdoGHiTpFD205UyTkOA1YCgJN8AyunCyKlkBbiWwD6yiDzdLyBJwLkHjugWlSciJBLwCu+jFVQnZF5t9B/YiNj8EioC4QmIbEdL5pc0eeTVVJaaQGCc5sZQK2QLu5cFZ5MbrwJ3sMRCLnBXz9U6IZUpISputEpNSxJSQJmy2TExqEd+E+DY7AjZIh18H/R+/Q8yglpBuwzbr30LKm1AhpMwAYsnvrzWvYm8FWrNq+/VrIrUYN68XYllhpxTjsm5Rsmoas2rjs/qwyuZTN6vhw6KMg54XeUDXBnpynoscRqaPsf2hliH2RG4iZZNrGIZhGAYa+AQu6kng3Jtj4AAAAABJRU5ErkJggg==" alt=""></button>
+        <button @click="sinClick" class="button_basic">sin</button>
+        <button @click="cosClick" class="button_basic">cos</button>
+        <button @click="tgClick" class="button_basic">tg</button>
+        <button @click="acrTgClick" class="button_basic">arctg</button>
+        <button @click="logClick" class="button_basic">log</button>
+        <button @click="lnClick" class="button_basic">ln</button>
+        <button @click="factorialClick" class="button_basic">!</button>
+        <button @click="powClick" class="button_basic">xʸ</button>
+        <button @click="reverseClick" class="button_basic">¹/x</button>
+        <button @click="sqrClick" class="button_basic">x²</button>
+        <button @click="sqrtClick" class="button_basic">²√x</button>
+        <button @click="divideClick" class="button_operation">÷</button>
+        <button @click="sevenClick" class="button_basic">7</button>
+        <button @click="eightClick" class="button_basic">8</button>
+        <button @click="nineClick" class="button_basic">9</button>
+        <button @click="multiplyClick" class="button_operation">×</button>
+        <button @click="fourClick" class="button_basic">4</button>
+        <button @click="fiveClick" class="button_basic">5</button>
+        <button @click="sixClick" class="button_basic">6</button>
+        <button @click="subtractClick" class="button_operation">-</button>
+        <button @click="oneClick" class="button_basic">1</button>
+        <button @click="twoClick" class="button_basic">2</button>
+        <button @click="threeClick" class="button_basic">3</button>
+        <button @click="plusClick" class="button_operation">+</button>
+        <button @click="expClick" class="button_basic">eˣ</button>
+        <button @click="zeroClick" class="button_basic">0</button>
+        <button @click="dotClick" class="button_basic">,</button>
+        <button @click="equalClick" class="button_basic button_equal">=</button>
       </div> 
     </div>
   </template>
@@ -56,7 +56,7 @@
               numLock: false,
               number1: 0.0,
               number2: 0.0,
-              numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
+              basic: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
               
           }
       },
@@ -171,7 +171,7 @@
         },
   
         mrClick() {
-          this.result = Number(this.memoryValue)
+          this.result += Number(this.memoryValue)
         },
   
         mplusClick() {
@@ -183,11 +183,11 @@
         },
   
         percentClick() {
-          const numbersArray = this.result.split(/(\+|\-|\*|\/|\^)/);
+          const basicArray = this.result.split(/(\+|\-|\*|\/|\^)/);
           if (this.result.includes('+') || this.result.includes('-') || this.result.includes('*') || this.result.includes('/') || this.result.includes('^')) {
-            const number1 = Number(numbersArray[0]);
-            const number2 = Number(numbersArray[2]);
-            const operator = numbersArray[1];
+            const number1 = Number(basicArray[0]);
+            const number2 = Number(basicArray[2]);
+            const operator = basicArray[1];
             const percentNum = number1 * number2 / 100;
             this.result = number1.toString() + operator + percentNum.toString();
           } 
@@ -284,7 +284,13 @@
         },
   
         subtractClick() {
-          this.result += '-'
+          if (!this.result.startsWith('0')) {
+            this.result += '-'
+          }
+          else {
+            this.result = ''
+            this.result += '-0.'
+          }
         },
   
         plusClick() {
@@ -300,10 +306,10 @@
         },
   
         equalClick() {
-          const numbersArray = this.result.split(/(\+|\-|\*|\/|\^)/)
-          const number1 = Number(numbersArray[0]) //число 1
-          const number2 = Number(numbersArray[2]) //число 2
-          const operation = numbersArray[1] //оператор
+          const basicArray = this.result.split(/(\+|\-|\*|\/|\^)/)
+          const number1 = Number(basicArray[0]) //число 1
+          const number2 = Number(basicArray[2]) //число 2
+          const operation = basicArray[1] //оператор
           switch(operation) {
             case '+': {
               this.result = number1 + number2;
@@ -359,7 +365,7 @@
       font-size: 26px;
     }
   
-    .calcInterface {
+    .calculator_interface {
       border: 2px solid black;
       border-radius: 10px;
       align-items: center;
@@ -381,7 +387,7 @@
       align-items: center;
     }
   
-    .butMemory {
+    .button_memory {
       background-color: rgb(134, 134, 134);
       border: 0.5px solid rgb(158, 157, 157);
       border-radius: 10px;
@@ -395,13 +401,13 @@
       box-shadow: 0px -5px 5px -5px rgba(34, 60, 80, 0.45) inset;
     }
   
-    .butMemory:hover {
+    .button_memory:hover {
       background-color:  rgb(187, 187, 187);
       color: black;
       cursor: pointer;
     }
   
-    .but {
+    .button_basic {
         background-color: white;
         border: 0.5px solid rgb(158, 157, 157);
         border-radius: 10px;
@@ -415,13 +421,13 @@
         box-shadow: 0px -5px 5px -5px rgba(34, 60, 80, 0.45) inset;
     }
   
-    .but:hover {
+    .button_basic:hover {
         background-color:   rgb(224, 224, 224);
         color: black;
         cursor: pointer;
     }
   
-    .operation {
+    .button_operation {
         background-color: orange;
         border: 0.5px solid rgb(158, 157, 157);
         border-radius: 10px;
@@ -435,28 +441,28 @@
         box-shadow: 0px -5px 5px -5px rgba(34, 60, 80, 0.45) inset;
     }
   
-    .operation:hover {
+    .button_operation:hover {
         background-color: rgb(255, 187, 60);
         color: black;
         cursor: pointer;
     }
   
-    .imgClear {
+    .symbol_image {
         width: 30px;
         height: 30px;
     }
   
-    .symbol {
+    .button_symbol {
         margin-bottom: 0;
         align-items: center;
     }
   
-    .equal {
+    .button_equal {
         background-color: rgb(50, 50, 253);
         color: white;
     }
   
-    .equal:hover {
+    .button_equal:hover {
         background-color: rgb(64, 64, 252);
         color: white;
     }
